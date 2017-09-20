3a9c6a17990ceba10b475d4e4bb81f97487de40b
Eduardo Blank Dias Moutinho <eduardo_blank2@hotmail.com>
2017-09-20 08:10:00 -0300

    Ty

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class App : Application
+	{
+        public static MasterDetailPage MasterDatail { get; set; }
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new App1.MainPage();
+			
+		}
+
+	
+	}
+}
